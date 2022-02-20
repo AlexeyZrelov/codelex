3d92298714c4df32c6f2cd7cb9760f0b5f8814a8
Alexey Zrelov <azheimdall@gmail.com>
2022-02-20 17:45:45 +0200

    update

--- web2/index.php
+++ web2/index.php
@@ -44,6 +44,18 @@ if (isset($_POST['delete'])) {
 
 }
 
+if (isset($_POST['save'])) {
+
+    try {
+        $conn->update('persons', array('person_name' => $_POST['name'], 'person_surname' => $_POST['sname'], 'person_kod' => $_POST['pkod']), array('idpersons' => $_POST['id']));
+    } catch (\Doctrine\DBAL\Exception $e) {
+        echo "Error!: " . $e->getMessage() . "\n";
+        die();
+    }
+
+    header("location: /");
+}
+
 try {
     $person = $conn->fetchAllAssociative('SELECT * FROM persons');
 } catch (\Doctrine\DBAL\Exception $e) {
@@ -64,12 +76,33 @@ try {
 <div style="width: 50%; margin: auto">
     <h4>SIGN UP</h4>
     <div>
-        <form class="form" action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+        <form class="form" action="" method="post">
             <div class="container">
+
+            <?php if (isset($_GET['edit'])) {
+
+                try {
+                    $person1 = $conn->fetchAssociative('SELECT * FROM persons WHERE idpersons = ?', array($_GET['id']));;
+                } catch (\Doctrine\DBAL\Exception $e) {
+                    echo "Error!: " . $e->getMessage() . "\n";
+                    die();
+                }
+            ?>
+                <input type='hidden' name='id' value='<?php echo $_GET['id']; ?>' />
+                <input type="text" placeholder="Name" name="name" value="<?php echo $person1['person_name']; ?>">
+                <input type="text" placeholder="Surname" name="sname" value="<?php echo $person1['person_surname']; ?>">
+                <input type="text" placeholder="Person kod" name="pkod" value="<?php echo $person1['person_kod']; ?>">
+                <button type="submit" name="save">SAVE</button>
+
+            <?php } else { ?>
+
                 <input type="text" placeholder="Name" name="name">
                 <input type="text" placeholder="Surname" name="sname">
                 <input type="text" placeholder="Person kod" name="pkod">
                 <button type="submit" name="submit">SIGN UP</button>
+
+            <?php } ?>
+
             </div>
         </form>
     </div>
@@ -81,7 +114,8 @@ try {
                 <th>Name</th>
                 <th>Surname</th>
                 <th>Person kode</th>
-                <th>...</th>
+                <th>✘</th>
+                <th>✎</th>
             </tr>
 
             <?php
@@ -104,9 +138,15 @@ try {
                     <?php echo $val['person_kod']; ?>
                 </td>
                 <td>
-                    <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+                    <form action="" method="post">
                         <input type='hidden' name='id' value='<?php echo $val['idpersons']; ?>' />
                         <button type="submit" name="delete">Delete</button>
+                    </form>
+                </td>
+                <td>
+                    <form action="" method="get">
+                        <input type='hidden' name='id' value='<?php echo $val['idpersons']; ?>' />
+                        <button type="submit" name="edit">Edit</button>
                     </form>
                 </td>
             </tr>
